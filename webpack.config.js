00af3bdfab878b9c89aeda554d7754f717ceaea9
태영 <fnkillur@naver.com>
2019-07-16 23:03:47 +0900

    setting webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader',   // translates CSS into CommonJS
+          'sass-loader'   // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+    
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, 'public/index.html'), // 없으면 빈 html을 만들어 생성됨.
+    })
+  ],
+};
