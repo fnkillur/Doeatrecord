b11459cdebab27eec3b3398b1fa0c48af6cc69c3
teo.dor <teo.dor@kakaocorp.com>
2019-08-05 11:15:53 +0900

    modify header css

--- src/pages/main/Header.js
+++ src/pages/main/Header.js
@@ -2,28 +2,23 @@ import React from "react";
 import "./Header.scss";
 import {getMe} from "../../_common/utils";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEdit, faWalking} from "@fortawesome/free-solid-svg-icons";
+import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
 
 const Header = () => {
-  const {nickname} = getMe();
-  
-  return (
-    <header className="header">
-      <div className="">
-        <Link to="/record">
-          <FontAwesomeIcon icon={faEdit}/>
-          <strong>Do Eat, Record!</strong>
-        </Link>
-      </div>
-      <p><strong>{nickname}</strong>님, 반갑습니다.</p>
-      <div className="view-icon">
-        <Link to="/me">
-          <FontAwesomeIcon icon={faWalking}/>
-        </Link>
-      </div>
-    </header>
-  );
+	const {nickname} = getMe();
+	
+	return (
+		<header className="header">
+			<Link to="/record">
+				<FontAwesomeIcon icon={faEdit}/>
+				<strong>  Do Eat, Record!</strong>
+			</Link>
+			<Link to="/me">
+				<strong>{nickname}</strong>
+			</Link>
+		</header>
+	);
 };
 
 export default Header;
