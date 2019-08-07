82d640326f1cdeccf6ae0409b3f1de3a5f770a13
태영 <fnkillur@naver.com>
2019-08-07 22:55:17 +0900

    modify Record Layout and add marker, overlay in Map

--- src/pages/record/Map.js
+++ src/pages/record/Map.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PropTypes from "prop-types";
 import "./Map.scss";
 
@@ -7,6 +7,7 @@ const places = new kakao.maps.services.Places();
 const Map = ({searchText}) => {
   const mapEl = useRef(null);
   const [map, setMap] = useState({});
+  const [info, setInfo] = useState({overlays: [], markers: [], length: 0});
   
   useEffect(() => {
     setMap(new kakao.maps.Map(mapEl.current, {
@@ -16,38 +17,63 @@ const Map = ({searchText}) => {
   }, [mapEl.current]);
   
   useEffect(() => {
-    const displayMarker = place => {
-      
-      const infoWindow = new kakao.maps.InfoWindow({
-        zIndex: 1,
-        removable: true,
-        content: `
-          <div style="padding:2px;font-size:12px;">${place.place_name}</div>
-          <button type="button" class="btn">기록하기</button>
-        `
-      });
-      
-      const marker = new kakao.maps.Marker({
-        map,
-        position: new kakao.maps.LatLng(place.y, place.x)
-      });
-      
-      kakao.maps.event.addListener(marker, "click", function () {
-        infoWindow.open(map, marker);
-      });
-    };
-    
     places.keywordSearch(searchText, (searchPlaces, status) => {
       if (status === kakao.maps.services.Status.OK) {
         
         let bounds = new kakao.maps.LatLngBounds();
         
-        for (let i = 0; i < searchPlaces.length; i++) {
-          displayMarker(searchPlaces[i]);
-          bounds.extend(new kakao.maps.LatLng(searchPlaces[i].y, searchPlaces[i].x));
+        const {overlays, markers, length} = info;
+        if (length) {
+          console.log(overlays);
+          overlays.forEach(overlay => overlay.setMap(null));
+          markers.forEach(marker => marker.setMap(null));
         }
         
-        map.setBounds(bounds);
+        setInfo(searchPlaces.reduce((acc, place) => {
+          const {x, y, place_name, address_name, road_address_name, phone, place_url} = place;
+          
+          const content = `
+            <div class="overlay">
+              <div class="info">
+                <div class="title">
+                  ${place_name}
+                </div>
+                <div class="body">
+                  <div class="desc">
+                    <div class="ellipsis">${road_address_name || address_name}</div>
+                    <div class="jibun ellipsis">${phone}</div>
+                    <div><a href="${place_url}" target="_blank" class="link">카카오맵 링크</a></div>
+                    <div class="button-box"><button type="button" class="btn btn-record" onclick="">기록하기</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+          
+          const marker = new kakao.maps.Marker({
+            map,
+            position: new kakao.maps.LatLng(y, x)
+          });
+          
+          const overlay = new kakao.maps.CustomOverlay({
+            content,
+            position: marker.getPosition()
+          });
+          
+          kakao.maps.event.addListener(marker, "click", function () {
+            overlay.setMap(map);
+          });
+          
+          bounds.extend(new kakao.maps.LatLng(y, x));
+          
+          acc.overlays.push(overlay);
+          acc.markers.push(marker);
+          acc.length++;
+          
+          return acc;
+        }, {overlays: [], markers: [], length: 0}));
+        
+        map.setBounds(bounds, 300, 50, 0, 50);
       }
     });
   }, [searchText]);
