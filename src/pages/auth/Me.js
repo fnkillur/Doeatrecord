ee270738dfa228ab9ad39abd2f436ffa4e55099d
태영 <fnkillur@naver.com>
2019-09-17 22:20:31 +0900

    modify user query

--- src/pages/auth/Me.js
+++ src/pages/auth/Me.js
@@ -11,65 +11,61 @@ import "./Me.scss";
 const GET_USERS = gql`
   query Users($keyword: String) {
     users(keyword: $keyword) {
-      Users {
-        userId
-        coupleId
-        nickname
-        thumbnail
-      }
+      userId
+      coupleId
+      nickname
+      thumbnail
     }
   }
 `;
 
 const Me = ({location: {search}, history}) => {
-	
-	const {nickname, thumbnail_image} = getMe();
-	
-	const {keyword} = queryString.parse(search);
-	const searchKeyword = keyword => history.push(`/main/me${keyword ? `?keyword=${keyword}` : ''}`);
-	
-	const {loading, error, data, fetchMore} = useQuery(GET_USERS, {variables: {keyword}});
-	
-	useEffect(() => {
-		// TODO: List.js 처럼 useQuery로 User 정보 조회
-		
-		
-	}, [keyword]);
-	
-	const {users} = data;
-	
-	return (
-		<main className="me">
-			<section className="profile-info">
-				<img src={thumbnail_image} className="profile-thumbnail-img"/>
-				<div className="profile-nickname"><strong>{nickname}</strong>님</div>
-				, 환영합니다.<Emoji text=":bow:"/>
-			</section>
-			<section className="me-search-friends">
-				<SearchBar
-					keyword={keyword}
-					searchKeyword={searchKeyword}
-					placeholder="나의 푸드메이트는 누구?"
-				/>
-				<div className="me-search-result">
-					{
-						loading && <ClipLoader size={50} color="white"/>
-					}
-					{
-						error && <>사용자 목록을 가져오는데 실패했어요.<Emoji text=":cry:"/></>
-					}
-					{
-						users.map(({useId, coupleId, nickname, thumbnail}) => (
-							<div className="profile-info">
-								<img scr={thumbnail} className="profile-thumbnail-img"/>
-								<div className="profile-nickname"><strong>{nickname}</strong></div>
-							</div>
-						))
-					}
-				</div>
-			</section>
-		</main>
-	);
+  
+  const {nickname, thumbnail_image} = getMe();
+  
+  const {keyword} = queryString.parse(search);
+  const searchKeyword = keyword => history.push(`/main/me${keyword ? `?keyword=${keyword}` : ''}`);
+  
+  const {loading, error, data, fetchMore} = useQuery(GET_USERS, {variables: {keyword}});
+  
+  useEffect(() => {
+    // TODO: List.js 처럼 useQuery로 User 정보 조회
+    
+    
+  }, [keyword]);
+  
+  return (
+    <main className="me">
+      <section className="profile-info">
+        <img src={thumbnail_image} className="profile-thumbnail-img"/>
+        <div className="profile-nickname"><strong>{nickname}</strong>님</div>
+        , 환영합니다.<Emoji text=":bow:"/>
+      </section>
+      <section className="me-search-friends">
+        <SearchBar
+          keyword={keyword}
+          searchKeyword={searchKeyword}
+          placeholder="나의 푸드메이트는 누구?"
+        />
+        <div className="me-search-result">
+          {
+            loading && <ClipLoader size={50} color="white"/>
+          }
+          {
+            error && <>사용자 목록을 가져오는데 실패했어요.<Emoji text=":cry:"/></>
+          }
+          {
+            !loading && !error && data.users.map(({useId, coupleId, nickname, thumbnail}) => (
+              <div className="profile-info">
+                <img src={thumbnail} className="profile-thumbnail-img" alt="썸네일"/>
+                <div className="profile-nickname"><strong>{nickname}</strong></div>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Me;
