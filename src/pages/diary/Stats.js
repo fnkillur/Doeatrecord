fb132d6a4a38693d994e00c4cdfce95d4447a5c6
태영 <fnkillur@naver.com>
2019-09-29 20:06:35 +0900

    modify stats month

--- src/pages/diary/Stats.js
+++ src/pages/diary/Stats.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from "react";
 import Spending from "../../components/Spending";
+import {faCaretLeft, faCaretRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./Stats.scss";
 
 const Stats = ({match: {params: {userId}}}) => {
+  
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  
   return (
     <main className="stats">
       <section className="stats-header">
-        <strong>{new Date().getMonth() + 1}월</strong>
+        <FontAwesomeIcon icon={faCaretLeft} onClick={() => setMonth(month - 1)}/>
+        <strong>{month}월</strong>
+        <FontAwesomeIcon icon={faCaretRight} onClick={() => setMonth(month + 1)}/>
       </section>
       <Spending userId={userId}/>
     </main>
